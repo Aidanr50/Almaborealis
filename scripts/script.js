5e6a365806a373484de61e9dcaf8b39e7a491243
Aidanr50 <20323198@nescol.ac.uk>
2020-10-29 00:28:52 +0000

    ADD Responsive sections & anim

--- scripts/script.js
+++ scripts/script.js
@@ -52,7 +52,25 @@ function hemChange() {
     hemTag.innerHTML = colour
 }
 
+//Get the button
+var mybutton = document.getElementById("myBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 function tabOpen(e, tab) {
     var i, tabcontent, tablinks;
@@ -69,4 +87,3 @@ function tabOpen(e, tab) {
   }
 
   document.getElementById("defaultOpen").click();
-  
