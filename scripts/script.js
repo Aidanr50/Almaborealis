ea4e12dc68bae168ce87d4f3a71391a641a09bc5
Aidanr50 <20323198@nescol.ac.uk>
2020-11-11 20:08:04 +0000

    ADD Modal video

--- scripts/script.js
+++ scripts/script.js
@@ -76,6 +76,29 @@ function changeStyle(style, part) {
   console.log(imageSrc)
 }
 
+// Video Modal Script
+
+function openModal() {
+  let modal = document.getElementById('modal');
+
+  modal.classList.remove('hidden')
+  modal.classList.add('flex')
+}
+
+const open = document.getElementById('open');
+open.addEventListener("click", openModal);
+
+function closeModal() {
+  let modal = document.getElementById('modal');
+
+  modal.classList.remove('flex')
+  modal.classList.add('hidden')
+  player.stopVideo();
+}
+
+const close = document.getElementById("close");
+close.addEventListener("click", closeModal);
+
 // Scroll to top Script
 
 var mybutton = document.getElementById("myBtn");
