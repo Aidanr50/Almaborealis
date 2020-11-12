e1dd604c585453715818e133664d3ece5c3db8d5
Aidanr50 <20323198@nescol.ac.uk>
2020-11-12 12:56:58 +0000

    EDIT modal script order

--- scripts/script.js
+++ scripts/script.js
@@ -76,29 +76,6 @@ function changeStyle(style, part) {
   console.log(imageSrc)
 }
 
-// Video Modal Script
-
-function openModal() {
-  let modal = document.getElementById('modal');
-
-  modal.classList.remove('hidden')
-  modal.classList.add('flex')
-}
-
-const open = document.getElementById('open');
-open.addEventListener("click", openModal);
-
-function closeModal() {
-  let modal = document.getElementById('modal');
-
-  modal.classList.remove('flex')
-  modal.classList.add('hidden')
-  player.stopVideo();
-}
-
-const close = document.getElementById("close");
-close.addEventListener("click", closeModal);
-
 // Scroll to top Script
 
 var mybutton = document.getElementById("myBtn");
@@ -167,3 +144,26 @@ function menuToggle() {
     nav.classList.remove('block')
   }
 }
+
+// Video Modal Script
+
+function openModal() {
+  let modal = document.getElementById('modal');
+
+  modal.classList.remove('hidden')
+  modal.classList.add('flex')
+}
+
+const open = document.getElementById('open');
+open.addEventListener("click", openModal);
+
+function closeModal() {
+  let modal = document.getElementById('modal');
+
+  modal.classList.remove('flex')
+  modal.classList.add('hidden')
+  player.stopVideo();
+}
+
+const close = document.getElementById("close");
+close.addEventListener("click", closeModal);
